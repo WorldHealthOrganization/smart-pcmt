620596526fa3853b0aef49e82597a279053144ec
Carl Leitner <litlfred@gmail.com>
2025-02-01 18:07:10 -0500

    added draft SM for creating ProductAuth from VaxprequalDB

--- datasets/vaxprequal/presushi.sh
+++ datasets/vaxprequal/presushi.sh
@@ -204,6 +204,7 @@ NR > 1{
   print "* index.value = \""MD5"\""
   print "* manufacturerReference = Reference(Manufacturer"MD5MANUFACTURER") "
   print "* responsibleNRAReference = Reference(Holder"MD5HOLDER") // "HOLDER
+  print "* vaxTypeCode = #SHORTVAXTYPE
   print ""    
   print "Instance: "SHORTVAXTYPE"Product"MD5
   print "InstanceOf: Product"
